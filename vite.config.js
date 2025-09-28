774b9937a90a19d04133ef615402f4fe46e56c2d
doanhhp <152097210+doanhhp@users.noreply.github.com>
2025-09-28 07:00:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 export default defineConfig({
-  base: "/8month",   // IMPORTANT for project repos
+  base: '/v2/',   // IMPORTANT for project repos
 })
